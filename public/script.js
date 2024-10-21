b96f0a5455b2a2a70b1db113d5b0283adf1d2e44
leartoler <fabiangg@protonmail.ch>
2024-10-21 14:12:59 -0500

    twetter url

---
+++ public/script.js
@@ -0,0 +1,29 @@
+const twitterUrl = "https://twitter.com/intent/tweet/";
+const linkTarget = '_top'
+const windowOptions = 'menubar=no,status=no,height=750,width=500'
+
+function extractTitleText() {
+  return document.querySelector('h1').innerText;
+}
+
+function extractAnchorLink() {
+  return document.querySelector('a').href;
+}
+
+function extractWindowLink() {
+  return window.location.href;
+}
+
+function openTwitterWindow(text, link) {
+  const twitterQuery = `text=${text}&url=${link}`;
+  return window.open(`${twitterUrl}?${twitterQuery}&`, linkTarget, windowOptions);
+}
+
+function registerShareButton() {
+  const text= extractTitleText()
+  const link = extractAnchorLink();
+  const twitterButton = document.querySelector('#button--twitter');
+  twitterButton.addEventListener('click', () => openTwitterWindow(text, link))
+}
+
+registerShareButton()
